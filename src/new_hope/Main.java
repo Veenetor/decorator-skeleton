485190523a23698780367c8a55961b3a151d1ccb
Andre Moura <apccfm@gmail.com>
2020-02-18 19:02:17 +0000

    added some comments

--- src/new_hope/Main.java
+++ src/new_hope/Main.java
@@ -14,13 +14,16 @@ public class Main {
 
         Menu newMenu = new Dessert(new BaseMenu());
         System.out.println("Menu: " + newMenu.getReceipt() + ", priced at " + newMenu.getCost() + " credits");
+        // this will print * Menu: Fried-Porg Wings, Bantha's Drool, priced at 6.5 credits *
 
         Menu altMenu = new Soda(new Dessert(new Coffee(new BaseMenu())));
         System.out.println("Menu: " + altMenu.getReceipt() + ", priced at " + altMenu.getCost() + " credits");
-
+        // this will print * Menu: Fried-Porg Wings, Coffee, Bantha's Drool, Blue milk Juice, priced at 8.0 credits *
 
         Menu classic = new Coke(new OldMenu());
         System.out.println("Menu: " + classic.getReceipt() + ", priced at " + classic.getCost() + " credits");
+        // this will print * Menu: Ol' Ben's classic Porg, Spice Coke, priced at 7.0 credits *
+
 
         // PVM END
     }
